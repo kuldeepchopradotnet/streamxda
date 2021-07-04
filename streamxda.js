8a6ebadc52c44b65eea6ee878eec85f06daf6998
Kuldeep Chopra <kuldeep.chopra@softobiz.com>
2021-07-04 17:48:22 +0530

    fixed script

--- streamxda.js
+++ streamxda.js
@@ -8,23 +8,36 @@ window.onload = () => {
         MAIN: 'main',
         CLASS: {
             MAIN_BLOG: 'main-blog'
-        }
+        },
+        ID: {
+            COMMENT: 'comments',
+            DISQUS: 'disqus_thread'
+        },
+        TAG: {
+            DIV: 'div',
+            SCRIPT: 'script'
+        },
+        ATTRIBUTE: {
+            DATE_STAMP: 'data-timestamp',
+            ID: 'id'
+        },
+        DISQUS_SRCIPT_URL: 'https://irobohawk.disqus.com/embed.js'
+
     }
 
 
     function disqusInit() {
-        var d = document, s = d.createElement('script'), div = d.createElement('div');
-        s.src = 'https://irobohawk.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', new Date().getTime().toString());
+        var d = document, c = d.getElementById(APP_CONST.ID.COMMENT);
+        if (!c) return;
+        var  s = d.createElement(APP_CONST.TAG.SCRIPT), div = d.createElement(APP_CONST.TAG.DIV);
+        s.src = APP_CONST.DISQUS_SRCIPT_URL;
+        s.setAttribute(APP_CONST.ATTRIBUTE.DATE_STAMP, new Date().getTime().toString());
         (d.head || d.body).appendChild(s);
-        div.setAttribute('id', 'disqus_thread');
-        var c = d.getElementById('comments');
-        //c.innerHTML = '';
+        div.setAttribute(APP_CONST.ATTRIBUTE.ID, APP_CONST.ID.DISQUS);
         c.appendChild(div);
         var disqus_config = function () {
             this.page.url = location.href;
             this.page.identifier = location.href;
-            console.log('disqus_config');
         };
     }
 
