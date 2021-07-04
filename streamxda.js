e5c98fd537e84dd13549b7374fb25a105e35f48c
Kuldeep Chopra <kuldeep.chopra@softobiz.com>
2021-07-04 16:33:06 +0530

    added font family

--- streamxda.js
+++ streamxda.js
@@ -7,16 +7,10 @@ window.onload = () => {
         }
     }
 
-    console.log('DOCUMENT LOADED');
-    const main = document.getElementById(APP_CONST.MAIN);
-    main.classList.add(APP_CONST.CLASS.MAIN_BLOG);
+    //console.log('DOCUMENT LOADED');
+    // const main = document.getElementById(APP_CONST.MAIN);
+    // main.classList.add(APP_CONST.CLASS.MAIN_BLOG);
 
 
-    var form = document.getElementById('searchthis')
-    var title = document.getElementById('blog-title')
-    title.appendChild(form)
-
-
-
-    console.log('SCRIPTED DONE');
+   // console.log('SCRIPTED DONE');
 }
