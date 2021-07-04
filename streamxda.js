dcc11f6390b5c6b49cdfad82b5fa3215dda19608
Kuldeep Chopra <kuldeep.chopra@softobiz.com>
2021-07-04 16:57:23 +0530

    added disqus

--- streamxda.js
+++ streamxda.js
@@ -1,5 +1,9 @@
-window.onload = () => {
 
+
+
+
+
+window.onload = () => {
     const APP_CONST = {
         MAIN: 'main',
         CLASS: {
@@ -7,10 +11,30 @@ window.onload = () => {
         }
     }
 
+
+    function disqusInit() {
+        var d = document, s = d.createElement('script'), div = d.createElement('div');
+        s.src = 'https://irobohawk.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', new Date().getTime().toString());
+        (d.head || d.body).appendChild(s);
+        div.setAttribute('id', 'disqus_thread');
+        var c = d.getElementById('comments');
+        c.appendChild(div);
+        var disqus_config = function () {
+            this.page.url = location.href;
+            this.page.identifier = location.href;
+            console.log('disqus_config');
+        };
+    }
+
+    disqusInit();
+
     //console.log('DOCUMENT LOADED');
     // const main = document.getElementById(APP_CONST.MAIN);
     // main.classList.add(APP_CONST.CLASS.MAIN_BLOG);
 
 
-   // console.log('SCRIPTED DONE');
+    // console.log('SCRIPTED DONE');
 }
+
+
--- streamxda.js.ts
+++ streamxda.js.ts
@@ -1,5 +1,9 @@
-(function () {
 
+
+
+
+
+window.onload = () => {
     const APP_CONST = {
         MAIN: 'main',
         CLASS: {
@@ -7,10 +11,30 @@
         }
     }
 
-    console.log('DOCUMENT LOADED');
 
-    const main = document.getElementById(APP_CONST.MAIN);
-    main.classList.add(APP_CONST.CLASS.MAIN_BLOG);
+    function disqusInit() {
+        var d = document, s = d.createElement('script'), div = d.createElement('div');
+        s.src = 'https://irobohawk.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', new Date().getTime().toString());
+        (d.head || d.body).appendChild(s);
+        div.setAttribute('id', 'disqus_thread');
+        var c = d.getElementById('comments');
+        c.appendChild(div);
+        var disqus_config = function () {
+            this.page.url = location.href;
+            this.page.identifier = location.href;
+            console.log('disqus_config');
+        };
+    }
 
-    console.log('SCRIPTED DONE');
-})();
+    disqusInit();
+
+    //console.log('DOCUMENT LOADED');
+    // const main = document.getElementById(APP_CONST.MAIN);
+    // main.classList.add(APP_CONST.CLASS.MAIN_BLOG);
+
+
+    // console.log('SCRIPTED DONE');
+}
+
+
