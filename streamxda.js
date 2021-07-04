3961a942bb47e8e35ca0604af704839189dba17f
Kuldeep Chopra <kuldeep.chopra@softobiz.com>
2021-07-04 16:20:07 +0530

    hide search

--- streamxda.js
+++ streamxda.js
@@ -6,9 +6,17 @@ window.onload = () => {
             MAIN_BLOG: 'main-blog'
         }
     }
-    
+
     console.log('DOCUMENT LOADED');
     const main = document.getElementById(APP_CONST.MAIN);
     main.classList.add(APP_CONST.CLASS.MAIN_BLOG);
+
+
+    var form = document.getElementById('searchthis')
+    var title = document.getElementById('blog-title')
+    title.appendChild(form)
+
+
+
     console.log('SCRIPTED DONE');
 }
